650fc02d7b63d09c9b75e2f523839139e6b29053
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-09 15:31:04 -0300

    A importância do raciocínio lógico

--- src/pages/Modulo1/Modulo1.jsx
+++ src/pages/Modulo1/Modulo1.jsx
@@ -72,6 +72,51 @@ const Modulo1 = () => {
         O processos de estruturar o pensamento seguindo normas e regras para
         resolver um problema.
       </p>
+      <br />
+      <hr />
+      <h2>A importância do raciocínio lógico</h2>
+      <p>
+        A comunicação é algo natural e para ela acontecer, é necessário uma
+        troca de mensagens entre um emissor e um receptor.
+      </p>
+      <p>
+        A coerência dessas mensagens está relacionada à lógica, pois ela observa
+        a ordem do pensamento, ajudando a compreender melhor as informações de
+        um diálogo. Sem a lógica, as trocas de informações não fariam sentido e
+        não haveria comunicação.
+      </p>
+      <h3>Raciocínio Lógico</h3>
+      <p>
+        A capacidade de organizar premissas válidas para chegar a uma conclusão
+        é tida como raciocínio lógico. Nós desenvolvemos essa habilidade, sempre
+        que precisamos resolver um problema, seguindo uma sequência de passos,
+        como um algoritmo. O raciocínio lógico é dividido em três tipos:
+        <strong> dedução, indução e abdução.</strong>
+      </p>
+      <p>
+        Na <strong>dedução</strong>, a conclusão é determinada utilizando a
+        regra e a premissa. Por exemplo: um triângulo é um polígono de três
+        lados. Logo, todo e qualquer polígono de três lados será um triângulo.
+      </p>
+      <p>
+        Diferente da dedução, na <strong>indução</strong>, a regra está
+        submetida aos dados, ou seja, é definida uma regra geral a partir das
+        informações. Por exemplo: em um teste de medicamento, a maioria dos
+        pacientes que receberam o tratamento apresentou uma melhora
+        considerável. Logo, o medicamento teve sua eficácia comprovada.
+      </p>
+      <p>
+        A <strong>abdução</strong> utiliza a conclusão e a regra para determinar
+        a premissa. Por exemplo: se há fumaça, há fogo. Eu vi fumaça, logo, aqui
+        tem fogo.
+      </p>
+      <h3>Situações do Contidiano</h3>
+      <p>
+        É comum a lógica ser aplicada em diversas situações do cotidiano, por
+        exemplo: “O carro está aquecendo. Logo, precisamos de água.” “João é
+        mais velho do que Ana. Ana é mais velha do que Lucas. Portanto, João é
+        mais velho que Lucas.”
+      </p>
     </div>
   );
 };
