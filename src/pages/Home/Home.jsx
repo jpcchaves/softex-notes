944650152af2a1baaba7bf2664e24f00ff5ed2c9
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-09 14:34:49 -0300

    home CSS updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,11 @@ import styles from './Home.module.css'
 const Home = () => {
   return (
     <div className={styles.container_home}>
-      <h1>Olá!</h1>
-      <p>
+      <h1 className={styles.title}>Olá!</h1>
+      <p className={styles.text}>
         Essa aplicação desenvolvida com <span>ReactJS</span> foi criada para
         realizar minhas anotações referentes ao curso de Front End ofertado pela
-        Softex!
-      </p>
-      <p>
+        Softex!<br/>
         Além disso, pretendo praticar minhas habilidades com ReactJS, aliando a
         teoria com a prática dos conhecimentos adquiridos ao longo do curso!
       </p>
