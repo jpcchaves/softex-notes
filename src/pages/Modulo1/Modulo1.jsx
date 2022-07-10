1ba0c5a30b784ebfe4afeaf1272cd882ff8df61d
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-10 11:01:02 -0300

    componentização dos módulos

--- src/pages/Modulo1/Modulo1.jsx
+++ src/pages/Modulo1/Modulo1.jsx
@@ -1,122 +1,14 @@
+// CSS
 import styles from "../Modules.module.css";
+// Módulos
+import Logica from "./01 Logica/Logica";
+import Raciocinio from "./02 Raciocinio/Raciocinio";
 
 const Modulo1 = () => {
   return (
     <div className={styles.module_container}>
-      <h2>Lógica de Programação e Orientação a Objetos</h2>
-      <p>
-        Geralmente o termo lógica está ligado a matemática e sua relação com
-        outras ciências. Porém, ela via muito além disso.
-      </p>
-      <p>
-        Lógica é a a correção do pensamento. A lógica nos ajuda a colocar a
-        nossa mente em ordem. Quando pensamos, escrevemos, falamos, estamos
-        utilizando a lógica, já que todas as tarefas necessitam de ordem no
-        pensamento.
-      </p>
-      <h3>Como surgiu a lógica?</h3>
-      <p>
-        Aristóteles definiu a lógica para verificar argumentos e a partir daí
-        diversos ouitros estudos surgiram.
-      </p>
-
-      <h4>Exemplos Práticos</h4>
-      <p>
-        1: Pegar um documento guardado na gaveta:É necessário abrir a gaveta
-        antes de pegá-lo (ordem do pensamento)
-      </p>
-      <p>
-        2: Quando queremos tomar um banho. É necessário abrir o registro do
-        chuveiro antes para que a água seja liberada e a partir daí tomar banho
-      </p>
-      <h4>Tipos de Lógica:</h4>
-      <p>
-        <strong>Lógica Informal</strong>: Utiliza uma linguagem mais natural,
-        informal
-      </p>
-      <p>
-        <strong>Lógica Formal</strong>: Expressa conceitos mais particulares da
-        língua, como definições
-      </p>
-      <p>
-        <strong>Lógica Simbólica</strong>: Trabalha com símbolos também voltada
-        para cálculos matemáticos
-      </p>
-      <p>
-        <strong>Lógica Matemática</strong> : Voltada para a teoria de conjuntos
-        e modelos que são seguidos na matemática
-      </p>
-
-      <h4>Afinal, o que é lógica de programação?</h4>
-      <p>
-        É a utilização da lógica para resolver problemas através de um software
-        ou aplicação. Quando falamos de programação, é necessário que tenhamos
-        consolidade todos os princípios dela. Pois, é justamente ela que nos
-        auxilia na criação dos algoritmos.
-      </p>
-      <h4>E o que são algoritmos?</h4>
-      <p>
-        Sequências de passos bem definidos para realizar determinado objetivo.
-      </p>
-      <p>
-        Todo passo a passo que executamos é considerado um algoritmo. A medida
-        que construímos essa sequência de passos, precisamos utilizar a lógica
-        para executar tudo em ordem.
-      </p>
-      <p>
-        Todas as tarefas executadas por um computador são baseadas em
-        algoritmos. Por isso, ao desenvolvermos esses algoritmos, além da lógica
-        de programação, precisamos pensar como um computador pensaria.
-      </p>
-      <p>
-        O processos de estruturar o pensamento seguindo normas e regras para
-        resolver um problema.
-      </p>
-      <br />
-      <hr />
-      <h2>A importância do raciocínio lógico</h2>
-      <p>
-        A comunicação é algo natural e para ela acontecer, é necessário uma
-        troca de mensagens entre um emissor e um receptor.
-      </p>
-      <p>
-        A coerência dessas mensagens está relacionada à lógica, pois ela observa
-        a ordem do pensamento, ajudando a compreender melhor as informações de
-        um diálogo. Sem a lógica, as trocas de informações não fariam sentido e
-        não haveria comunicação.
-      </p>
-      <h3>Raciocínio Lógico</h3>
-      <p>
-        A capacidade de organizar premissas válidas para chegar a uma conclusão
-        é tida como raciocínio lógico. Nós desenvolvemos essa habilidade, sempre
-        que precisamos resolver um problema, seguindo uma sequência de passos,
-        como um algoritmo. O raciocínio lógico é dividido em três tipos:
-        <strong> dedução, indução e abdução.</strong>
-      </p>
-      <p>
-        Na <strong>dedução</strong>, a conclusão é determinada utilizando a
-        regra e a premissa. Por exemplo: um triângulo é um polígono de três
-        lados. Logo, todo e qualquer polígono de três lados será um triângulo.
-      </p>
-      <p>
-        Diferente da dedução, na <strong>indução</strong>, a regra está
-        submetida aos dados, ou seja, é definida uma regra geral a partir das
-        informações. Por exemplo: em um teste de medicamento, a maioria dos
-        pacientes que receberam o tratamento apresentou uma melhora
-        considerável. Logo, o medicamento teve sua eficácia comprovada.
-      </p>
-      <p>
-        A <strong>abdução</strong> utiliza a conclusão e a regra para determinar
-        a premissa. Por exemplo: se há fumaça, há fogo. Eu vi fumaça, logo, aqui
-        tem fogo.
-      </p>
-      <h3>Situações do Contidiano</h3>
-      <p>
-        É comum a lógica ser aplicada em diversas situações do cotidiano, por
-        exemplo: “O carro está aquecendo. Logo, precisamos de água.” “João é
-        mais velho do que Ana. Ana é mais velha do que Lucas. Portanto, João é
-        mais velho que Lucas.”
-      </p>
+      <Logica />
+      <Raciocinio />
     </div>
   );
 };
