ba5503367bf2724082cc4b992b8470072f80448b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-16 11:56:39 -0300

    button visibility | changed to hooks

--- src/components/ButtonTop/ButtonTop.jsx
+++ src/components/ButtonTop/ButtonTop.jsx
@@ -1,11 +1,45 @@
+// Hooks
+import { useEffect, useState } from "react";
+// Utils
+import { classNames } from "../../utils/classNames";
+// CSS Module
 import styles from "./ButtonTop.module.css";
 
 const ButtonTop = () => {
+  const [isVisible, setVisibility] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setVisibility(true);
+    } else {
+      setVisibility(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
     <>
-      <a href="#navbar-topo" id={styles.topo}>
+      <button
+        type="button"
+        className={classNames(isVisible ? styles.topo : "")}
+        onClick={scrollToTop}
+      >
         &#9650;
-      </a>
+      </button>
     </>
   );
 };
